1912acbb6b82dbe93629df19eb72c59a60bec2c7
rare1k <hi@rare1k.dev>
2024-05-05 10:52:57 -0400

    feat: allow individual filters

--- src/web/api.ts
+++ src/web/api.ts
@@ -26,9 +26,30 @@ app.post("/check/:url/results.txt",async ({ params,body,set })=>{
     }
 
     let url = params.url
+    if (bj.filter == "all") {
     let results = [await filters.fortiguard(url),await filters.lightspeed(url),await filters.palo(url)]
     let formatted = ["FortiGuard:\nCategory: " + results[0] + "\n",`Lightspeed:\nLS Filter: ${results[1][0]}\nLS Rocket: ${results[1][1]}\n`,`Palo Alto:\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`]
     return `FilterChecker Report for ${url}:\n\n${formatted.join("\n")}`
+    } else {
+        let results; let formatted;
+        switch (bj.filter) {
+            case ("fortiguard" || "forti"):
+                results = await filters.fortiguard(url)
+                formatted = "FortiGuard:\nCategory: " + results[0]
+                return `FilterChecker Report for ${url}:\n\n${formatted}`
+            case ("lightspeed" || "ls"):
+                results = await filters.lightspeed(url)
+                formatted = `Lightspeed:\nLS Filter: ${results[0]}\nLS Rocket: ${results[1]}\n`
+                return `FilterChecker Report for ${url}:\n\n${formatted}`
+            case ("palo" || "paloalto") :
+                results = await filters.palo(url)
+                formatted = `Palo Alto:\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`
+                return `FilterChecker Report for ${url}:\n\n${formatted}`
+            default:
+                set.status = 400
+                return "Unknown filter. Accepted values: fortiguard, forti, lightspeed, ls, palo, paloalto"
+        }
+    }
 })
 
 
