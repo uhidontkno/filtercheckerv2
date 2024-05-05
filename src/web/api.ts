f18908063b99bd981a7380469778b5bc8af262a9
rare1k <hi@rare1k.dev>
2024-05-05 09:04:54 -0400

    feat: add functionality for checking all filters using api

--- src/web/api.ts
+++ src/web/api.ts
@@ -1,10 +1,18 @@
 import Elysia from "elysia";
+import filters from "../modules/filters"
 const app = new Elysia();
 app.get("/",()=>{
     return "OK"
 })
 app.get("/ping",()=>{
     return "OK"
+})
+
+app.get("/check/:url",async ({ params })=>{
+    let url = params.url
+    let results = [await filters.fortiguard(url),await filters.lightspeed(url),await filters.palo(url)]
+    let formatted = ["FortiGuard\nCategory: " + results[0] + "\n",`Lightspeed\nLS Filter: ${results[1][0]}\nLS Rocket: ${results[1][1]}\n`,`Palo Alto\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`]
+    return formatted.join("\n")
 })
 
 console.log(`Listening on port: ${process.env.port || 10000}`)
