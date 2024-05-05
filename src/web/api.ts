c8a210ef776b2004b64b0025b9b150107dd7e5b3
rare1k <hi@rare1k.dev>
2024-05-05 09:07:45 -0400

    feat: formatting

--- src/web/api.ts
+++ src/web/api.ts
@@ -11,8 +11,8 @@ app.get("/ping",()=>{
 app.get("/check/:url",async ({ params })=>{
     let url = params.url
     let results = [await filters.fortiguard(url),await filters.lightspeed(url),await filters.palo(url)]
-    let formatted = ["FortiGuard\nCategory: " + results[0] + "\n",`Lightspeed\nLS Filter: ${results[1][0]}\nLS Rocket: ${results[1][1]}\n`,`Palo Alto\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`]
-    return formatted.join("\n")
+    let formatted = ["FortiGuard:\nCategory: " + results[0] + "\n",`Lightspeed:\nLS Filter: ${results[1][0]}\nLS Rocket: ${results[1][1]}\n`,`Palo Alto:\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`]
+    return `FilterChecker Report for  ${url}:\n\n${formatted.join("\n")}`
 })
 
 console.log(`Listening on port: ${process.env.port || 10000}`)
