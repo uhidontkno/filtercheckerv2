f684f4f12340952b5a8e98784acdb5b831b912ec
rare1k <hi@rare1k.dev>
2024-05-05 09:10:32 -0400

    feat: allow changing the port via command line

--- src/web/api.ts
+++ src/web/api.ts
@@ -12,8 +12,8 @@ app.get("/check/:url",async ({ params })=>{
     let url = params.url
     let results = [await filters.fortiguard(url),await filters.lightspeed(url),await filters.palo(url)]
     let formatted = ["FortiGuard:\nCategory: " + results[0] + "\n",`Lightspeed:\nLS Filter: ${results[1][0]}\nLS Rocket: ${results[1][1]}\n`,`Palo Alto:\nRisk: ${results[2][1]}\nCategory: ${results[2][0].trim().replace(/^\s*$(?:\r\n?|\n)/gm,"")}\n`]
-    return `FilterChecker Report for  ${url}:\n\n${formatted.join("\n")}`
+    return `FilterChecker Report for ${url}:\n\n${formatted.join("\n")}`
 })
 
-console.log(`Listening on port: ${process.env.port || 10000}`)
-app.listen(process.env.port || 10000)
+console.log(`Listening on port: ${process.argv[3] || process.env.PORT || 10000}`)
+app.listen(process.argv[3] || process.env.PORT || 10000)
